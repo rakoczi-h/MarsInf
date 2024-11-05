150bd6611653a6417be152e3fd81b6032930ec79
Henrietta Rakoczi <rakoczi.henrietta@hotmail.com>
2024-11-05 09:12:43 +0000

    fixed shaping of the array to comply with rest of pipeline

--- marsinf/read_files.py
+++ marsinf/read_files.py
@@ -2,7 +2,7 @@ import pickle as pkl
 import numpy as np
 import os
 
-def read_files(data_location, filenames, model_parameters_to_include=['e_c', 'k_c', 'v_c'], noise=None):
+def read_files(data_location, filenames, datasize, model_parameters_to_include=['e_c', 'k_c', 'v_c'], noise=None):
     """
     Function that read the files defined by data_location and filenames. This is specific for data files describing planets, which contain a dictionary with the parameters and the gravity of planets. Note that SH degrees below 2 will automatically be ignored.
     Parameters
@@ -27,7 +27,7 @@ def read_files(data_location, filenames, model_parameters_to_include=['e_c', 'k_
     for f in filenames:
         with open(os.path.join(data_location, f), 'rb') as file:
             dt = pkl.load(file)
-            td = np.vstack([dt[key] for key in model_parameters_to_include]).T
+            td = [dt[key] for key in model_parameters_to_include]
             # removing sh degrees below 2
             degrees = dt['sh_degrees']
             idx = np.argwhere(degrees[:,0]<2)
@@ -35,18 +35,41 @@ def read_files(data_location, filenames, model_parameters_to_include=['e_c', 'k_
             for i in idx:
                 tc = np.delete(tc, i, 1)
             num_deg_ord = np.shape(tc)[1]
-            tc = tc.reshape(tc.shape[0], -1)
+            tc = [tc[:,:,0], tc[:,:,1]]
             train_data.append(td)
             train_conditional.append(tc)
         print(f"{f} read")
-    train_data = np.vstack(train_data)
-    train_conditional = np.vstack(train_conditional)
+
+    tc_list = []
+    for i in range(len(train_conditional[0])):
+        tc = []
+        for j in range(len(train_conditional)):
+            tc.append(train_conditional[j][i])
+        tc = np.vstack(tc)[:datasize]
+        if len(np.shape(tc)) == 1:
+            tc = np.expand_dims(tc, axis=1)
+        tc_list.append(tc)
+    tc = []
+    train_conditional = tc_list
+
+    td_list = []
+    for i in range(len(train_data[0])):
+        td = []
+        for j in range(len(train_data)):
+            td.append(train_data[j][i])
+
+        td = np.hstack(td)[:datasize]
+        if len(np.shape(td)) == 1:
+            td = np.expand_dims(td, axis=1)
+        td_list.append(td)
+    td = []
+    train_data = td_list
 
     if noise is not None:
         noise = noise[:num_deg_ord,2:]
-        noise_simulation = np.random.normal(loc=0.0, scale=noise, size=(np.shape(train_conditional)[0],)+np.shape(noise))
-        noise_simulation = noise_simulation.reshape(noise_simulation.shape[0], -1)
-        train_conditional = train_conditional + noise_simulation
+        noise_simulation = np.random.normal(loc=0.0, scale=noise, size=(np.shape(train_conditional[0])[0],)+np.shape(noise))
+        train_conditional[0] = train_conditional[0] + noise_simulation[:,:,0]
+        train_conditional[1] = train_conditional[1] + noise_simulation[:,:,1]
         print("Added noise to the conditional...")
-    return [train_data], [train_conditional]
 
+    return train_data, train_conditional
